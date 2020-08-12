45cab0fdca603805ab3c5db0346aba6dc12a0169
MuhRehman <paki_jan@yahoo.com>
2020-08-13 02:35:17 +0500

    React test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <div className="">
+        <div className="container">
+          <div className="login-content">
+            <div className="login-logo pt-5">
+              <h4 className="text-center" style={{ color: "#ffffff" }}>
+                King Burger <font style={{ color: "#bfbfbf" }}>Admin</font>
+              </h4>
+            </div>
+
+            <div className="login-form">
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label style={{ color: "#bfbfbf" }}>
+                    EMAIL ADDRESS
+                  </Form.Label>
+                  <Form.Control placeholder=" Email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label style={{ color: "#bfbfbf" }}>PASSWORD</Form.Label>
+                  <Form.Control placeholder="Password" />
+                </Form.Group>
+                <Button
+                  name="login"
+                  type="submit"
+                  block
+                  className="btn btn-success btn-flat m-b-30 m-t-30"
+                >
+                  SIGN IN
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
